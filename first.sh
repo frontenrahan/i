c30a8bae666b1ae6bc1657597d0a8617b4c3ea87
Abu Taleb <106797791+frontenrahan@users.noreply.github.com>
2023-07-17 04:24:39 +0600

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,10 @@
+#Default network connection
+sudo echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+echo "Nameserver configuration was done..."
+sudo echo "deb http://kali.download/kali kali-rolling main contrib non-free\ndeb-src http://kali.download/kali kali-rolling main contrib non-free\n\n#deb http://kali.download/kali kali-last-snapshot main contrib non-free\n#deb-src http://kali.download/kali kali-last-snapshot main contrib non-free\n\n#BD alternative domain- http://kali.cs.nctu.edu.tw" | sudo tee -a /etc/apt/sources.list
+echo "Source list configuration was done..."
+#Cheking or updating linux
+sudo apt-get update && apt-get upgrade -y | echo "[Update & Upgrade done]"
+sudo apt-get install wget -y
+sudo wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add
+echo "apt-key configuration was done..."
